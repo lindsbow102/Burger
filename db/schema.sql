2ce9bbdcd2fe5a94953b61af87afbedc18ecb46f
Lindsey Bowen <lindsbow102@yahoo.com>
2018-04-26 17:34:17 -0700

    setup complete

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+-- Create the database burgers_db and specified it for use.
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+-- Create the table plans.
+CREATE TABLE burgers
+(
+id int NOT NULL AUTO_INCREMENT NOT NULL,
+burger_name varchar(255) NOT NULL,
+devoured BOOLEAN DEFAULT false,
+PRIMARY KEY (id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Bacon Avocado Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES('Cheddar Jalapano Guacamole Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Chipotle Turkey Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES('Taco Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Sweet & Spicy Hawaiian Burger', true);
